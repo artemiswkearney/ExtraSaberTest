69b06ad179b2d6ecf1b873fafbb5b737a85d0934
artemiswkearney <artemis.wk@gmail.com>
2020-02-25 12:37:23 -0800

    Add project files.

---
+++ ExtraSaberTest/Plugin/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Utilities;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+using Harmony;
+using System.Reflection;
+
+namespace Plugin
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void Init(IPALogger logger)
+        {
+            Logger.log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+            var harmony = HarmonyInstance.Create("com.arti.BeatSaber.Plugin");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void OnApplicationQuit()
+        {
+            Logger.log.Debug("OnApplicationQuit");
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+
+        }
+    }
+}
